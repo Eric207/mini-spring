b27c61090af1c22351b4556a3c8352f4b64feadd
Eric207 <512095059@qq.com>
2024-06-04 16:11:40 +0800

    提交测试.

--- src/main/java/org/springframework/core/io/ClassPathResource.java
+++ src/main/java/org/springframework/core/io/ClassPathResource.java
@@ -20,6 +20,8 @@ public class ClassPathResource implements Resource {
 
 	@Override
 	public InputStream getInputStream() throws IOException {
+		//this.getClass().getClassLoader():classLoader是一个类装载器,正常用于读取硬盘中.class文件的字节码.存入内存让其成为对象
+		//在这里通过getResourceAsStream 获得一个InputStream流,通过path读取对应classpath下的文件
 		InputStream is = this.getClass().getClassLoader().getResourceAsStream(this.path);
 		if (is == null) {
 			throw new FileNotFoundException(this.path + " cannot be opened because it does not exist");
